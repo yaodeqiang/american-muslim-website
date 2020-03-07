035e8b7017d23a59fe019a41a093a9081bce8efe
要德强 <17635723756@163.com>
2020-03-07 14:07:23 +0800

    update

---
+++ src/components/home/content/content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.module.scss';
+
+class Content extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Content
---
+++ src/components/home/swiperAnimate/swiperCom.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.module.scss';
+
+class SwiperCom extends Component {
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SwiperCom
